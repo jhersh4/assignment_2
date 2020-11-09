ff3656bb19c0ef4f99d7db2cfe5b41f74438e5ab
jhersh4 <70652247+jhersh4@users.noreply.github.com>
2020-11-08 22:08:35 -0500

    updates

--- public/script.js
+++ public/script.js
@@ -12,5 +12,28 @@ function search (query, restarray){
     return restarray.filter(result => {
         const expression = new RegExp(query, 'gi')
         return result.name.match(expression) || result.city.match(expression)
-    })
-}
+    })};
+
+function showmatches() {
+    const matches = search(this.value,restarray);
+    let index = matches.map(restaurant => {
+        const expression = new RegExp(this.value,'gi');
+        return `
+        <li>
+        <span class="restname">${restaurant.name} || </span>
+        <span class="restcat">${restaurant.category} || </span>
+        <span class="restcity">${restaurant.city}</span>
+        </li>
+        `;
+    }).join('');
+    if(this.value.length === 0) {
+        index = [];}
+    searchresults.innerHTML = index;
+};
+
+const searchinput = document.querySelector('.searchInput')
+const searchresults = document.querySelector('.ulList')
+
+searchinput.addEventListener('change', showmatches);
+
+searchinput.addEventListener('keyup', showmatches);
